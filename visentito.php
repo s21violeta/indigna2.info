a3d21605b7f3b9cd562afbdec1aba082120de3b7
maara gonzalez <maara.gonzalez@gmail.com>
2018-06-08 17:57:39 +0200

    version maara

--- visentito.php
+++ visentito.php
@@ -9,15 +9,5 @@ echo 'pastel de zanahoria listo para comer';
 echo 'ok';
 
 echo 'hacedor de tiramisues';
-<<<<<<< HEAD
 
-=======
-
-echo 'Pero hoy no ha traido ninguno';
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/s21violeta/indigna2.info.git
-=======
-
-echo 'cocina otro tiramisu visentito!';
->>>>>>> branch 'master' of https://github.com/s21violeta/indigna2.info.git
 ?>
